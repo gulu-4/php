78ce02f89ae536a3d9c2924ab7c27f8835884d15
gulu-4 <2768819370@qq.com>
2021-12-23 18:24:22 +0800

    添加-项目初始化

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "hello world!";
+
+$num = 2;
+switch (num)
+{
+    case 1:
+        echo "1";
+        break;
+    case 2:
+        echo "2";
+        break;
+    default:
+        echo "3";
+}
